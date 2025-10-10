f44941c049a3c3512163738816660375ecec2782
Javivis <javiermapel10@gmail.com>
2025-10-09 19:29:26 -0600

    Mi primer script
    En este script se encuentran mis primeros comandos

---
+++ mi primer script.R
@@ -0,0 +1,21 @@
+#Hola
+#aquí va el codigo 
+
+
+
+
+# cargar matriz de datos iris en 
+# el objeto BD que significa
+# Base de Datos
+BD<-iris
+
+# este comando sirve para visualizar 
+# una matriz de datos
+View(BD)
+
+str(iris)
+#comando función precargada con esos datos
+str(BD)
+#str lo que se encarga es visualiza y arroja el tipo de variable
+#existen dos tipos de variables son cuantitativas y cualitativas como los factores
+
